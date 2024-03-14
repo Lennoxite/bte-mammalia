3003b21b9b683034a3f68b5fcfd7ee6fb5004c16
Lennoxicon <pinoy1999@gmail.com>
2024-03-13 19:51:26 -0700

    1.5 modding

---
+++ 1.5/Source/BiotechExpansion_Mammalia/CompProperties_AbilityAntleredHeadbutt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Verse;
+using RimWorld;
+namespace BTE_MMLA
+{
+
+	public class CompProperties_AbilityAntleredHeadbutt : CompProperties_AbilityEffect
+	{
+
+		public CompProperties_AbilityAntleredHeadbutt()
+		{
+			this.compClass = typeof(CompAbilityEffect_AntleredHeadbutt);
+		}
+
+
+	}
+}
---
+++ 1.5/Source/BiotechExpansion_Mammalia/HediffComp_ExplodeOnDeath.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using RimWorld;
+using Verse;
+namespace BTE_MMLA
+{
+	public class HediffComp_ExplodeOnDeath : HediffComp
+	{
+		public HediffCompProperties_ExplodeOnDeath Props
+		{
+			get
+			{
+				return (HediffCompProperties_ExplodeOnDeath)this.props;
+			}
+		}
+
+		public override void Notify_PawnDied()
+		{
+			Corpse owner = this.parent.pawn.Corpse;
+			if (Props.damageType == null)
+				Props.damageType = DamageDefOf.Flame;
+			if (owner != null)
+				GenExplosion.DoExplosion(owner.Position, owner.Map, Props.radius, Props.damageType, owner.InnerPawn, -1, -1f, null, null, null, null, null, 0f, 1, null, false, null, 0f, 1, 0f, false, null, null, null, true, 1f, 0f, true, null, 1f);
+		}
+
+	}
+}
