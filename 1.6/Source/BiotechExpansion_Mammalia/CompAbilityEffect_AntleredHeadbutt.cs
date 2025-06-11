e4bdbd1f4f3d14abad9d3d2e9d990f6a69d7a927
Lennoxicon <pinoy1999@gmail.com>
2025-06-11 15:07:32 -0700

    1.6

---
+++ 1.6/Source/BiotechExpansion_Mammalia/CompAbilityEffect_AntleredHeadbutt.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Verse;
+using RimWorld;
+namespace BTE_MMLA
+{
+
+	public class CompAbilityEffect_AntleredHeadbutt : CompAbilityEffect
+	{
+
+		public new CompProperties_AbilityAntleredHeadbutt Props
+		{
+			get
+			{
+				return (CompProperties_AbilityAntleredHeadbutt)this.props;
+			}
+		}
+
+
+		public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
+		{
+			base.Apply(target, dest);
+            if (target.Thing is Pawn pawn)
+            {
+                pawn.stances.stunner.StunFor(8f.SecondsToTicks(), parent.pawn, addBattleLog: false);
+            }
+        }
+
+
+		public override bool CanApplyOn(LocalTargetInfo target, LocalTargetInfo dest)
+		{
+			return this.Valid(target, false);
+		}
+
+
+		public override bool Valid(LocalTargetInfo target, bool throwMessages = false)
+		{
+			Pawn pawn = target.Pawn;
+			if (pawn == null)
+			{
+				return false;
+			}
+			//if (!AbilityUtility.ValidateMustBeHumanOrWildMan(pawn, throwMessages, this.parent))
+			//{
+			//	return false;
+			//}
+
+			return true;
+		}
+		public override bool AICanTargetNow(LocalTargetInfo target)
+		{
+			return Valid(target, false);
+		}
+	}
+}
---
+++ 1.6/Source/BiotechExpansion_Mammalia/HediffComp_ExplodeOnDeath.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using RimWorld;
+using Verse;
+namespace BTE_MMLA
+{
+	public class HediffComp_ExplodeOnDeath : HediffComp
+	{
+		public HediffCompProperties_ExplodeOnDeath Props
+		{
+			get
+			{
+				return (HediffCompProperties_ExplodeOnDeath)this.props;
+			}
+		}
+
+		public override void Notify_PawnDied(DamageInfo? dinfo, Hediff culprit = null)
+		{
+			Corpse owner = this.parent.pawn.Corpse;
+			if (Props.damageType == null)
+				Props.damageType = DamageDefOf.Flame;
+			if (owner != null)
+				GenExplosion.DoExplosion(owner.Position, owner.Map, Props.radius, Props.damageType, owner.InnerPawn, -1, -1f, null, null, null, null, null, 0f, 1, null, null, 0, false, null, 0f, 0, 0, false, null, null, null, true, 1, 0, true);
+		}
+
+	}
+}
